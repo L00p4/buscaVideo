ccb689dcfc1b07ec28f76e328bdcb4e8e5c0e94e
Guilherme Marucchi <guilherme.marucchi@gmail.com>
2023-04-27 08:30:54 -0300

    subindo index.html e js/

---
+++ js/conectaApi.js
@@ -0,0 +1,39 @@
+async function lsitaVideos() {
+    const conexao = await fetch('http://localhost:3000/videos');
+    const conexaoConvertida = await conexao.json();
+    
+    return conexaoConvertida
+}
+
+async function criaVideo(titulo, descricao, url, imagem){
+    const conexao = await fetch('http://localhost:3000/videos', {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url, url,
+            imagem: imagem
+        })
+    });
+
+    const conexaoConvertida = await conexao.json();
+    
+    return conexaoConvertida;
+
+}
+
+async function buscaVideo(termoBusca) {
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoBusca}`);
+    const conexaoConvertida = conexao.json();
+
+    return conexaoConvertida;
+}
+
+export const conectaApi = {
+    lsitaVideos,
+    criaVideo,
+    buscaVideo
+}
